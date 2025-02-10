793df5d0a142a27d8c70d632d24ce82474c4fc99
YoungBeen <rhdudqls73@gmail.com>
2025-02-10 17:03:02 +0900

    m

--- docs/app.js
+++ docs/app.js
@@ -1,23 +1,72 @@
 async function main() {
-  function hanldeCC() {
-    function hanldeCC() {
-      document.querySelector("#box").textContent = await("");
+  async function hanldeCC(event) {
+    event.preventDefault(); // Form의 기본 submit 막아줘야하고...
+
+    // 로딩을 추가했다가...
+    const spinner = document.createElement("div");
+    spinner.classList.add("spinner-border");
+    document.querySelector("#box").appendChild(spinner);
+
+    const url = "http://127.0.0.1:3000";
+    const formData = new FormData(document.querySelector("#ccForm"));
+    const text = formData.get("text");
+    // console.log(text);
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        text,
+      }),
+      // Content-Type 꼭!
+      headers: {
+        "Content-Type": "Application/json",
+      },
+    });
+    const json = await response.json();
+
+    // 로딩을 추가했다가 종료시 없애는...
+    spinner.remove();
+
+    // document.querySelector("#box").textContent = JSON.stringify(json);
+
+    const { image, desc } = json;
+
+    const box = document.querySelector("#box");
+    box.innerHTML = "";
+    const imageTag = document.createElement("img");
+    imageTag.classList.add("img-fluid");
+    imageTag.src = image; // image - link
+    const descTag = document.createElement("p");
+    descTag.textContent = desc;
+    box.appendChild(imageTag);
+    box.appendChild(descTag);
+  }
+
+  //   document.querySelector("#ccBtn").addEventListener("click", hanldeCC);
+  document.querySelector("#ccForm").addEventListener("submit", hanldeCC);
+}
+
+document.addEventListener("DOMContentLoaded", main);
+
+// -------
+
+async function callAI({ url, model, text, apiKey }) {
+  const response = await axios.post(
+    url,
+    {
+      model,
+      messages: [
+        {
+          role: "user",
+          content: text,
+        },
+      ],
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+        "Content-Type": "application/json",
+      },
     }
-
-    document.querySelector("#ccBtn").addEventListener("click", hanldeCC);
-  }
+  );
+  return response.data;
 }
-
-document.addEventListener("DOMContentLoaded", main);
-async function main() {
-  async function hanldeCC() {
-    // cors 때문에 (port가 달라서) 오류 나는 코드 -> CORS를 통해 해결
-    document.querySelector("#box").textContent = await (
-      await fetch("http://127.0.0.1:3000")
-    ).text();
-  }
-
-  document.querySelector("#ccBtn").addEventListener("click", hanldeCC);
-}
-
-document.addEventListener("DOMContentLoaded", main);
